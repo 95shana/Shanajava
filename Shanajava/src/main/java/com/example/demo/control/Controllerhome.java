b5558320a7e9ed10e32ab6e35425b95106d43f0e
Nitu <Zaiy123ohm@gmail.com>
2022-11-08 10:05:57 +0900

    first commit

---
+++ Shanajava/src/main/java/com/example/demo/control/Controllerhome.java
@@ -0,0 +1,27 @@
+package com.example.demo.control;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.mpdel.Usershana;
+
+@Controller
+
+public class Controllerhome {
+	@GetMapping("/")
+	public String shaa() {
+		return "shana";
+		
+	}
+	@PostMapping("/send")
+	public String hghg(Usershana user ,Model model) {
+		model.addAttribute("title","Success page");
+		model.addAttribute("data",user);
+		return "success";
+	
+	}
+
+}
---
+++ Shanajava/src/main/java/com/example/demo/mpdel/Usershana.java
@@ -0,0 +1,19 @@
+package com.example.demo.mpdel;
+
+import lombok.Data;
+
+@Data
+
+public class Usershana {
+	private String name;
+	private String email;
+	private String password;
+	private String Age;
+	private String address;
+	private String number;
+	private String alu;
+	private String malefemale;
+	
+	
+
+}
